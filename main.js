398fbb927a113aa60aa145bd95e52ab21912e188
Charles Bayley <cbayley95@gmail.com>
2018-05-18 23:39:38 -0400

    Added stamp Point

--- main.js
+++ main.js
@@ -2,3 +2,13 @@
 
 const stampit = require('stampit');
 
+const Point = stampit()
+  .props({ x: 0, y: 0 })
+  .init(function({ x = this.x, y = this.y }) {
+    this.x = x;
+    this.y = y;
+  });
+
+const p1 = Point({ x: 1, y: 1 });
+console.log(p1);
+
