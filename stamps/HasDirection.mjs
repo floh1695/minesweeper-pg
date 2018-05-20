6062d6e4d84de42e07bbb418a6f202bc8f59dcd9
Charles Bayley <cbayley95@gmail.com>
2018-05-19 21:17:56 -0400

    More functional

--- stamps/HasDirection.mjs
+++ stamps/HasDirection.mjs
@@ -2,7 +2,7 @@ import _ from 'lodash';
 import stampit from 'stampit';
 
 function createHasDirectionStamp() {
-  const directionStamps = [];
+  // const directionStamps = [];
 
   const directions = [
     'north',
@@ -15,17 +15,17 @@ function createHasDirectionStamp() {
     'northWest',
   ];
 
-  directions.forEach((direction) => {
+  const directionStamps = _.map(directions, function (direction) {
     const props = {};
     props[direction] = null;
 
     const upperDirection = direction[0].toUpperCase() + direction.slice(1);
     const methods = {};
-    methods[`set${upperDirection}`] = function setter(itemAt) {
+    methods[`set${upperDirection}`] = function (itemAt) {
       this[direction] = itemAt;
       return this;
     };
-    methods[`get${upperDirection}`] = function getter() {
+    methods[`get${upperDirection}`] = function () {
       return this[direction];
     };
 
@@ -33,8 +33,29 @@ function createHasDirectionStamp() {
       .props(props)
       .methods(methods);
 
-    directionStamps.push(directionStamp);
+    return directionStamp;
   });
+
+  // _.each(directions, function (direction) {
+  //   const props = {};
+  //   props[direction] = null;
+
+  //   const upperDirection = direction[0].toUpperCase() + direction.slice(1);
+  //   const methods = {};
+  //   methods[`set${upperDirection}`] = function (itemAt) {
+  //     this[direction] = itemAt;
+  //     return this;
+  //   };
+  //   methods[`get${upperDirection}`] = function () {
+  //     return this[direction];
+  //   };
+
+  //   const directionStamp = stampit()
+  //     .props(props)
+  //     .methods(methods);
+
+  //   directionStamps.push(directionStamp);
+  // });
 
   return directionStamps.reduce((fullStamp, nextStamp) => fullStamp.compose(nextStamp));
 }
